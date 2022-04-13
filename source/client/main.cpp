371753e84dfb2c63fefeb6a18275f9624766c32c
S.Klapproth <severikl@student.ethz.ch>
2022-04-13 17:15:22 +0200

    minor stuff

--- source/client/main.cpp
+++ source/client/main.cpp
@@ -3,3 +3,25 @@
 
 // Macro which will generate a main function which serves as entry point into the program
 wxIMPLEMENT_APP(Lama);
+
+
+
+
+/*
+L->
+bet
+GameControl
+response listener thread
+blackjack
+
+
+
+K->
+connectionPael
+maingamep
+GUI Window
+:::
+
+
+header and skeleton
+*/
