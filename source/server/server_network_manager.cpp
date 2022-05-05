bc51d525c79079957313256af8a031259f7105db
Pascal Engeler <engelerp@phys.ethz.ch>
2022-05-05 17:03:13 +0200

    fixed three typos

--- source/server/server_network_manager.cpp
+++ source/server/server_network_manager.cpp
@@ -2,15 +2,15 @@
 #include "request_handler.hpp"
 
 #include "../general/network/default.conf"
-#include "../general/network/responses/request_response.hpp"
+#include "../general/network/responses/server_response.hpp"
 
 server_network_manager::server_network_manager(){
   //If there's no server_network_manager yet, initialize
   if(_instance == nullptr){
-    _instance = *this;
+    _instance = this;
   }
   sockpp::socket_initializer socket_initializer;
-  this->connect(default_server_host, default_port)
+  this->connect(default_server_host, default_port);
 }
 
 server_network_manager::~server_network_manager() = default;
