22820de9c6dac6432f923dd09b97f633ac6fc006
noec <noec@ethz.ch>
2022-05-06 11:38:19 +0200

    changed a few filename in CMake.txt to compile

---
+++ cmake-build-debug/googletest/lib/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/noec/Documents/scuola/ETH/FS_22/Software Engineering/git/blackjack/googletest/lib/googlemock
+# Build directory: /home/noec/Documents/scuola/ETH/FS_22/Software Engineering/git/blackjack/cmake-build-debug/googletest/lib/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
