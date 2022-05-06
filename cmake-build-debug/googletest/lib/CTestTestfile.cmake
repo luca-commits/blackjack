8b0795ff66ca801df67d7010ebd0f7c6407ec432
noec <noec@ethz.ch>
2022-05-06 18:41:58 +0200

    test if gitignore works

---
+++ cmake-build-debug/googletest/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/noec/Documents/scuola/ETH/FS_22/Software Engineering/git/blackjack/googletest/lib
+# Build directory: /home/noec/Documents/scuola/ETH/FS_22/Software Engineering/git/blackjack/cmake-build-debug/googletest/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
