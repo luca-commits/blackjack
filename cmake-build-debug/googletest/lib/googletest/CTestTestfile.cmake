a7d79991daaf7a6de86df5375cb6f5a8d610b836
noec <noec@ethz.ch>
2022-05-06 18:40:38 +0200

    removed cmake build again

--- cmake-build-debug/googletest/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/noec/Documents/scuola/ETH/FS_22/Software Engineering/git/blackjack/googletest/lib/googletest
-# Build directory: /home/noec/Documents/scuola/ETH/FS_22/Software Engineering/git/blackjack/cmake-build-debug/googletest/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
