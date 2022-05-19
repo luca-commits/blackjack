d05786207104c06b3d546574a2b8fea9769012ba
Pascal Engeler <engelerp@phys.ethz.ch>
2022-05-19 15:43:59 +0200

    Fixed losing criterion server side

--- source/general/game_state/player.cpp
+++ source/general/game_state/player.cpp
@@ -94,12 +94,12 @@ void player::setup_round(std::string& err) {
 // based on how many points the dealer has, chooses to call won_round, lost_round or draw round
 void player::wrap_up_round(int dealer_points, std::string& err) {
     int player_points = _player_hand->get_points(err);
-    if(player_points > dealer_points) {
+    if(player_points > 21 || player_points < dealer_points){
+        this->lost_round();
+    } else if(player_points > dealer_points) {
         this->won_round();
     } else if(player_points == dealer_points) {
         this->draw_round();
-    } else if(player_points < dealer_points) {
-        this->lost_round();
     } else {
         err = "Something went wrong during wrapping round for player " + this->_player_name->get_value();
     }
